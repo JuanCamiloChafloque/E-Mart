d7a815765409d22e157a97a01494f05090f2e058
Camilo <camilochafloque@gmail.com>
2022-04-17 09:23:57 -0500

    Created Header and Footer components layouts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/layouts/Header";
+import Footer from "./components/layouts/Footer";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route exact path="/" element={<h1>Hello</h1>} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ frontend/src/components/layouts/Header.js
@@ -0,0 +1,51 @@
+import React, { Fragment } from "react";
+
+const Header = () => {
+  return (
+    <Fragment>
+      <nav className="navbar row">
+        <div className="col-12 col-md-3">
+          <div className="navbar-brand">
+            <strong style={{ color: "#ffffff" }}>E-Mart</strong>
+            <img
+              src="images/logo.png"
+              style={{ height: "30px", marginLeft: "5px" }}
+              alt=""
+            />
+          </div>
+        </div>
+
+        <div className="col-12 col-md-6 mt-2 mt-md-0">
+          <div className="input-group">
+            <input
+              type="text"
+              id="search_field"
+              className="form-control"
+              placeholder="Enter Product Name ..."
+            />
+            <div className="input-group-append">
+              <button id="search_btn" className="btn">
+                <i className="fa fa-search" aria-hidden="true"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
+          <button className="btn" id="login_btn">
+            Login
+          </button>
+
+          <span id="cart" className="ml-3">
+            Cart
+          </span>
+          <span className="ml-1" id="cart_count">
+            2
+          </span>
+        </div>
+      </nav>
+    </Fragment>
+  );
+};
+
+export default Header;
