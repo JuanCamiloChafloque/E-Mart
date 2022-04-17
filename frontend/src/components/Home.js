67be0c2df10d195f69ad92d497ca6801bff3ac85
Camilo <camilochafloque@gmail.com>
2022-04-17 16:44:34 -0500

    Loader component

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useEffect } from "react";
 import MetaData from "./layouts/MetaData";
 import Product from "./products/Product";
+import Loader from "./layouts/Loader";
 
 import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../actions/productsActions";
@@ -16,18 +17,24 @@ const Home = () => {
   }, [dispatch]);
 
   return (
-    <Fragment>
-      <MetaData title="Welcome! Buy the best products online" />
-      <h1 id="products_heading">Latest Products</h1>
-      <section id="products" className="container mt-5">
-        <div className="row">
-          {products &&
-            products.map((product) => (
-              <Product key={product._id} product={product} />
-            ))}
-        </div>
-      </section>
-    </Fragment>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <MetaData title="Welcome! Buy the best products online" />
+          <h1 id="products_heading">Latest Products</h1>
+          <section id="products" className="container mt-5">
+            <div className="row">
+              {products &&
+                products.map((product) => (
+                  <Product key={product._id} product={product} />
+                ))}
+            </div>
+          </section>
+        </Fragment>
+      )}
+    </>
   );
 };
 
